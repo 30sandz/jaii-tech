b29c0e94e2a613c3aef35e0a3768c406d326b3ba
SMI <91237098+sandeep-manivannan@users.noreply.github.com>
2023-04-30 09:40:33 +0530

    done

---
+++ web/static/js/header.js
@@ -0,0 +1,10 @@
+function Opennav() {
+    document.getElementById("nav").style.cssText = "position:relative;left:0vw;"
+    document.getElementById("bar").style.cssText = "visibility: hidden;"
+    document.getElementById("x").style.cssText = "visibility: visible"
+}
+function Closenav() {
+    document.getElementById("nav").style.cssText = "position:relative;left:100vw;"
+    document.getElementById("bar").style.cssText = "visibility:visible;"
+    document.getElementById("x").style.cssText = "visibility: hidden;"
+}
---
+++ web/static/js/main.js
@@ -0,0 +1,59 @@
+import * as THREE from "https://cdn.jsdelivr.net/npm/three@0.121.1/build/three.module.js";
+import {OrbitControls} from "https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js";
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/loaders/GLTFLoader.js';
+
+const scene = new THREE.Scene();
+console.log(scene)
+
+const loader = new GLTFLoader();
+loader.load('./assets/scene.gltf',function(gltf){
+    console.log(gltf)
+    const root = gltf.scene;
+    root.scale.set(5,5,5);
+    scene.add(root);
+} );
+
+const light = new THREE.DirectionalLight(0xffffff,1);
+light.position.set(5,1,1)
+scene.add(light);
+
+const backlight = new THREE.DirectionalLight(0xffffff,2);
+backlight.position.set(-5,-1,-1);
+scene.add(backlight)
+
+const sizes = {
+    width: window.innerWidth/3,
+    height: window.innerHeight/1.3,
+
+}
+
+const camera = new THREE.PerspectiveCamera(45,sizes.width / sizes.height,0.1,100);
+camera.position.x = 20;
+scene.add(camera);
+
+const canvas = document.querySelector(".model");
+const renderer = new THREE.WebGL1Renderer( {canvas, alpha:true  });
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(2);
+renderer.render(scene,camera);
+
+
+const controls = new OrbitControls(camera,canvas)
+controls.enableDamping = true
+controls.enablePan = false
+controls.enableZoom = false
+
+
+window.addEventListener("resize", () => {
+    sizes.width = window.innerWidth/3
+    sizes.height = window.innerHeight/1.3
+    camera.aspect = sizes.width/sizes.height
+    renderer.setSize(sizes.width,sizes.height)
+});
+const loop = () => {
+    controls.update()
+    renderer.render(scene,camera)
+    window.requestAnimationFrame(loop)
+}
+loop()
+    
